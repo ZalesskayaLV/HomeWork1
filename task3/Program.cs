cf9719dbaf389354d27999e7e3ec6538ff3e92cd
Lyubov Zalesskaya <zalesskayalyubov@gmail.com>
2022-04-29 21:32:20 +0400

    Intermediate result

---
+++ task3/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Напишите программу, которая на вход
+принимает число (N), а на выходе показывает все чётные
+числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= number)
+{
+    if (count%2==0)
+    {
+        Console.Write(count);
+    } 
+    count++;   
+        if (count!=number)
+        {
+      Console.Write(", "); 
+        }   
+}
+
+/*
+for (int i/2 == 0; i <= number; i++)
+{
+    Console.Write(i);
+    if (i != number)
+    {
+        Console.Write(", ");
+    }
+}
+*/
